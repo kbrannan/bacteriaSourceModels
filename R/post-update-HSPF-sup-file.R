6bebd5ae01a6a765c152d75825256af02345ffcb
kbrannan <brannan.kevin@deq.state.or.us>
2017-07-18 16:41:18 -0700

    used for-loop to midify the sup file (that is read into a character vector)

--- R/post-update-HSPF-sup-file.R
+++ R/post-update-HSPF-sup-file.R
@@ -3,12 +3,13 @@
 #' @param chr.file.sup is the file name for the HSPF-sup-file (includes path if necessary)
 #' @export
 
-post.update.HSPF.sup.file <- function(row.pls.line.info, chr.hspf.sup, df.sup.table, chr.sup.col.num.name) {
+post.update.HSPF.sup.file <- function(df.pls.line.info, chr.hspf.sup, df.sup.table, chr.sup.col.num.name) {
 
   chr.hspf.sup.update <- chr.hspf.sup
-  row.cur <- grep(paste0(df.sup.table[row.pls.line.info, chr.sup.col.num.name], "( ){1,}12$"), chr.hspf.sup.update) + 1
-  chr.hspf.sup[row.cur] <- paste0("  ",
-                                  paste0(sprintf(fmt = "%.8E",
-                                                 df.accum.sup.out[row.pls.line.info, 4:15]), collapse = " "))
-  return(chr.hspf.sup)
+  num.rows <- 1:length(df.pls.line.info[, 1])
+  for(ii in num.rows) {
+    row.cur <- grep(paste0(df.sup.table[ii, chr.sup.col.num.name], "( ){1,}12$"), chr.hspf.sup.update) + 1
+    chr.hspf.sup.update[row.cur] <- paste0("  ", paste0(sprintf(fmt = "%.8E", df.sup.table[ii, 4:15]), collapse = " "))
+  }
+  return(chr.hspf.sup.update)
 }
