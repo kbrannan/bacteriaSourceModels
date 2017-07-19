542d8c2995731e62d5b883cd05fe06b2447f4f81
kbrannan <brannan.kevin@deq.state.or.us>
2017-07-19 12:17:37 -0700

    changled call for reshape function to include stats package name

--- R/post-merge-pls-info-to-sup-table.R
+++ R/post-merge-pls-info-to-sup-table.R
@@ -5,19 +5,19 @@
 #' @param chr.sup.col.num.name the column name in df.sup.table that has the sup-file line numbers for the loads
 #' @export
 
-post.merge.pls.info.to.sup.table <- function(df.pls.line.info, df.df.sup.table, chr.sup.col.num.name) {
+post.merge.pls.info.to.sup.table <- function(df.pls.line.info, df.sup.table, chr.sup.col.num.name) {
 
   df.pls.line.info.merge <- cbind(df.pls.line.info, merge.me = paste0(as.character(df.pls.line.info$sub),
                                                                       tolower(as.character(df.pls.line.info$pls.name))))
 
-  df.table.merge <- cbind(df.df.sup.table,
-                          merge.me = paste0(as.character(df.df.sup.table$sub),
-                                            tolower(as.character(df.df.sup.table$PLS))))
+  df.table.merge <- cbind(df.sup.table,
+                          merge.me = paste0(as.character(df.sup.table$sub),
+                                            tolower(as.character(df.sup.table$PLS))))
 
   df.table.cmb <- merge(df.pls.line.info.merge, df.table.merge)
   rows.dup <- duplicated(df.table.cmb[, c("Month", "merge.me")])
   df.table.sup <- df.table.cmb[rows.dup == FALSE, c("sub", "pls.name", chr.sup.col.num.name, "Month", "load")]
-  df.table.sup.out <- reshape(df.table.sup[, ], timevar = "Month", direct = "wide",
+  df.table.sup.out <- stats::reshape(df.table.sup[, ], timevar = "Month", direct = "wide",
                               idvar = c("sub", "pls.name", chr.sup.col.num.name))
   return(df.table.sup.out)
 }
