abbb7e9fd9b333cb88b3c37e39a1243d7eeebc30
kbrannan <brannan.kevin@deq.state.or.us>
2017-07-24 15:45:59 -0700

    added commands to get sub watershed number from file name and changed what is returned to a data frame with sub watershed number ad file name

--- R/post-get-mutsin-file-names.R
+++ R/post-get-mutsin-file-names.R
@@ -1,16 +1,18 @@
 #' gets the names of the mutsin files from the HSPF uci file
-#' returns a character vector with the file names (without path if included)
+#' returns data.frame with the file names (without path if included) and sub watershed number
 #'
 #' @param chr.dir.hspf path to loaction of hspf input files and where hspf is run
 #' @param chr.file.uci name of hspf-uci input file
 #' @export
 
 post.get.mutsin.file.names <- function(chr.dir.hspf, chr.file.uci) {
+  options(stringsAsFactors = FALSE)
   chr.hspf.uci <- scan(file = paste0(chr.dir.hspf, "/", chr.file.uci), sep = "\n",
                      what = "character", quiet = TRUE)
-chr.files.block <- chr.hspf.uci[grep("^FILES", chr.hspf.uci):grep("^END FILES", chr.hspf.uci)]
+  chr.files.block <- chr.hspf.uci[grep("^FILES", chr.hspf.uci):grep("^END FILES", chr.hspf.uci)]
+  chr.file.names.mut <- stringr::str_extract(grep(".*\\.mut$", chr.files.block, value = TRUE), "[aA-zZ0-9]{1,}\\.mut")
+  df.files.mut <- data.frame(sub = stringr::str_extract(chr.file.names.mut, "[0-9]{1,}"),
+                             file = chr.file.names.mut)
 
-chr.file.names.mut <- stringr::str_extract(grep(".*\\.mut$", chr.files.block, value = TRUE), "[aA-zZ0-9]{1,}\\.mut")
-
-  return(chr.file.names.mut)
+  return(df.files.mut)
 }
